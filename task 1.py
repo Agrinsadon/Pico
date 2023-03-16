ad1d28af09739ba52de6212a83e6196045aad9ac
Agrinsadon <111856786+Agrinsadon@users.noreply.github.com>
2023-03-16 12:00:35 +0200

    Exercise 1. Pins and LEDs (1-2 done)

--- task 1.py
+++ task 1.py
@@ -1,7 +1,6 @@
 #Task 1: 1-2
-from machine import Pin
-
 #1.1
+from machine import Pin
 import time
 
 while True:
@@ -21,3 +20,62 @@ while True:
     D3.value(0)
 
 #1.2
+from machine import Pin
+import time
+from machine import Pin
+import time
+
+while True:
+    D1 = machine.Pin(22, machine.Pin.OUT)
+    D2 = machine.Pin(21, machine.Pin.OUT)
+    D3 = machine.Pin(20, machine.Pin.OUT)
+
+    time.sleep(1)
+
+    # 000
+    D1.value(0)
+    D2.value(0)
+    D3.value(0)
+
+    # 001
+    time.sleep(1)
+    D1.value(1)
+    D2.value(0)
+    D3.value(0)
+
+    # 010
+    time.sleep(1)
+    D1.value(0)
+    D2.value(1)
+    D3.value(0)
+
+    # 011
+    time.sleep(1)
+    D1.value(1)
+    D2.value(1)
+    D3.value(0)
+
+    # 100
+    time.sleep(1)
+    D1.value(0)
+    D2.value(0)
+    D3.value(1)
+
+    # 101
+    time.sleep(1)
+    D1.value(1)
+    D2.value(0)
+    D3.value(1)
+
+    # 110
+    time.sleep(1)
+    D1.value(0)
+    D2.value(1)
+    D3.value(1)
+
+    # 111
+    time.sleep(1)
+    D1.value(1)
+    D2.value(1)
+    D3.value(1)
+
